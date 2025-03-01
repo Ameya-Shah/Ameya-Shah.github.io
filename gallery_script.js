d8c7d1c2af97bbddf9eb18b7699c747515ff36f6
Ameya-Shah <ameyashah.25@gmail.com>
2025-03-01 11:31:12 -0800

    Added gallery script

---
+++ gallery_script.js
@@ -0,0 +1,7 @@
+const imageGallery = [
+    "https://images.pexels.com/photos/11424548/pexels-photo-11424548.jpeg?auto=compress&cs=tinysrgb&w=600"
+    "https://images.pexels.com/photos/5109341/pexels-photo-5109341.jpeg?auto=compress&cs=tinysrgb&w=600"
+    "https://images.pexels.com/photos/5123016/pexels-photo-5123016.jpeg?auto=compress&cs=tinysrgb&w=600"
+    "https://images.pexels.com/photos/2782485/pexels-photo-2782485.jpeg?auto=compress&cs=tinysrgb&w=600"
+    "https://vancouver.ca/images/cov/feature/vancouver-aquatic-centre-landing.jpg"
+]
