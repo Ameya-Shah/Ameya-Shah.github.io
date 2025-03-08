459c79afc98668f1ef03774a6e10a78d17d01fb1
Ameya-Shah <ameyashah.25@gmail.com>
2025-03-08 12:16:58 -0800

    Champions Trophy 2025

--- gallery_script.js
+++ gallery_script.js
@@ -1,19 +1,26 @@
 const imageGallery = [
-    "https://images.pexels.com/photos/11424548/pexels-photo-11424548.jpeg?auto=compress&cs=tinysrgb&w=600",
-    "https://images.pexels.com/photos/5109341/pexels-photo-5109341.jpeg?auto=compress&cs=tinysrgb&w=600",
-    "https://images.pexels.com/photos/5123016/pexels-photo-5123016.jpeg?auto=compress&cs=tinysrgb&w=600",
-    "https://images.pexels.com/photos/2782485/pexels-photo-2782485.jpeg?auto=compress&cs=tinysrgb&w=600",
-    "https://vancouver.ca/images/cov/feature/vancouver-aquatic-centre-landing.jpg",
+    "https://c.ndtvimg.com/2025-02/dv92otp_virat-kohli-_625x300_23_February_25.jpg?im=FeatureCrop,algorithm=dnn,width=806,height=605",
+    "https://images.news18.com/ibnlive/uploads/2025/02/india-vs-pakistan-live-streaming-champions-trophy-2025-2025-02-2e315419db0aa1f5603c623f99992ffc-16x9.jpg?impolicy=website&width=640&height=360",
+    "https://images.firstpost.com/uploads/2025/03/Champions-trophy-final-reserve-day-2025-03-3d90bbaf2891165292a24216b359a7b9-1200x675.jpg?im=FitAndFill=(596,336)",
+    "https://www.hindustantimes.com/ht-img/img/2025/03/04/550x309/rahul_jadeja_getty_1741104924146_1741104939355.jpg",
+    "https://c.ndtvimg.com/2025-03/ml59v3dg_team-australia-afp_625x300_04_March_25.jpg?im=FitAndFill,algorithm=dnn,width=806,height=605",
 ]
-
-const imageElement = document.getElementById
-("gallery-image");
+const imageText = [
+    "Virat Kohli makes 100 on the last ball, winning the Ind vs Pak match for India ",
+    "The India vs Pakistan match, arguably the greatest sporting rivalry in the world",
+    "New Zealand Cricketer Glenn Philips attempts to direct hit run-out Hardik Pandya from India ",
+    "KL Rahul and Ravindra Jadeja celebrate their partnership against Australia",
+    "Adam Zampa celebrates taking a Pakistani wicket"
+]
+const imageElement = document.getElementById("gallery-image");
 const prevButton = document.getElementById("prev-btn");
 const nextButton = document.getElementById("next-btn");
+const caption = document.getElementById("caption");
 
 let currentImgIndex = 0;
 
 function updateImage() {
+    caption.innerHTML = imageText [currentImgIndex];
     imageElement.src = imageGallery [currentImgIndex];
 }
 
