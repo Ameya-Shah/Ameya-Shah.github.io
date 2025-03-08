68f616f0b4a7c2bb63fcc58d64e7fe9e90662cfb
Ameya-Shah <ameyashah.25@gmail.com>
2025-03-08 10:39:00 -0800

    Added JS variables

--- gallery_script.js
+++ gallery_script.js
@@ -5,3 +5,9 @@ const imageGallery = [
     "https://images.pexels.com/photos/2782485/pexels-photo-2782485.jpeg?auto=compress&cs=tinysrgb&w=600"
     "https://vancouver.ca/images/cov/feature/vancouver-aquatic-centre-landing.jpg"
 ]
+
+const imageElement = document.getElementById("gallery-image");
+const prevButton = document.getElementById("prev-btn");
+const nextButton = document.getElementById("next-btn");
+
+let currentImgIndex = 0;
